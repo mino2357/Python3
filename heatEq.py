b969647506d49a40f99f54881bb5345f3f339dc1
mino2357 <mino2357@gmail.com>
2016-12-19 01:22:19 +0900

    animation

--- heatEq.py
+++ heatEq.py
@@ -3,12 +3,13 @@
 
 import math
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 D = 1.
 dim = 33
 dx = 1. / (dim-1)
 dt = (1. / (2. * D)) * dx * dx
-INTV = 10000
+INTV = 3000
 
 def initFunc(x):
 	a = 1./2
@@ -22,19 +23,25 @@ def nextStep(u):
 
 # initial condition
 
+fig = plt.figure()
+ims = []
+
 u = [0] * dim
 for i in range(0, dim):
 	u[i] = initFunc(i * dx)
 
 # time loop
 t = 0.
-i=0
+i = 0
 
 while t < 100.:
 	u = nextStep(u)
 	t = t + dt;
 	if i % INTV == 0:
-		plt.plot(u)
+		im = plt.plot(u,"b")
+		ims.append(im)
 	i = i + 1;
 
+ani = animation.ArtistAnimation(fig, ims)
+ani.save("heat.gif", writer = "imagemagick")
 plt.show()
