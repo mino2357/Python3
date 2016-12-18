6c6140becf4be50e81b7fc4cff302a29e8eba452
mino2357 <mino2357@gmail.com>
2016-12-18 23:22:39 +0900

    visualize

--- heatEq.py
+++ heatEq.py
@@ -2,14 +2,17 @@
 # u_t = u_xx x in [0, pi]
 
 import math
+import matplotlib.pyplot as plt
 
 D = 1.
-dim = 10
+dim = 33
 dx = 1. / (dim-1)
 dt = (1. / (2. * D)) * dx * dx
+INTV = 10000
 
 def initFunc(x):
-	return math.sin(x)
+	a = 1./2
+	return math.exp( - 100 * (x - a) * (x - a))
 
 def nextStep(u):
 	uNext = [0] * dim
@@ -21,13 +24,17 @@ def nextStep(u):
 
 u = [0] * dim
 for i in range(0, dim):
-	u[i] = initFunc(i * dx * math.pi)
+	u[i] = initFunc(i * dx)
 
 # time loop
 t = 0.
+i=0
 
-while t < 10.:
+while t < 100.:
 	u = nextStep(u)
 	t = t + dt;
-	print(u)
+	if i % INTV == 0:
+		plt.plot(u)
+	i = i + 1;
 
+plt.show()
